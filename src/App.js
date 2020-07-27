188e42bb1b5516578c33aeebd6a72d54fd8fe168
Angel Baro <angelbaro44@gmail.com>
2020-07-27 16:14:52 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Switch, Route, Link } from 'react-router-dom';
 import countrys from './countries.json';
